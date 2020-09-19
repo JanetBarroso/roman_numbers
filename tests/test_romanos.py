c65de488fd5547a56965dfe719d65f57219ba379
JanetBarroso <47856788+JanetBarroso@users.noreply.github.com>
2020-09-19 12:48:07 +0200

    includes an error msg for negatives and zero and tests

--- tests/test_romanos.py
+++ tests/test_romanos.py
@@ -1,8 +1,12 @@
 from romanos import romans
-
+import pytest
 def test_romans_for_one_is_I():
     assert romans(1) == "I"
 
 def test_romans_for_5_to_V():
     assert romans(5) == "V"
 
+def test_romans_for_negatives_or_zero():
+    assert romans(-1) == "not possible to convert " \
+                         "-1 to romans"
+    assert romans(0) == "not possible to convert 0 to romans"
