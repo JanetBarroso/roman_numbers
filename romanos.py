4fee584902686674cbe2fd1dae7ca673a9f1e5b0
JanetBarroso <47856788+JanetBarroso@users.noreply.github.com>
2020-09-23 18:50:30 +0200

    gives roman number for defined equivalents and for 1,2,3

--- romanos.py
+++ romanos.py
@@ -1,7 +1,12 @@
 def romans(entero):
     if entero <= 0:
-        return "not possible to convert " +\
-                         str(entero)+ " to romans"
+        return "not possible to convert " + \
+               str(entero) + " to romans"
 
-    equivalents = {1: "I", 5: "V"}
-    return equivalents[entero]
+    equivalents = {1: "I", 5: "V", 10: "X", 50: "L",
+                   100: "C", 500: "D", 1000: "M"}
+    if entero <= 3:
+        roman = equivalents[1] * entero
+    else:
+        roman = equivalents[entero]
+    return roman
--- tests/test_romanos.py
+++ tests/test_romanos.py
@@ -1,12 +1,24 @@
 from romanos import romans
-import pytest
+
+
 def test_romans_for_one_is_I():
     assert romans(1) == "I"
 
+
 def test_romans_for_5_to_V():
     assert romans(5) == "V"
+
 
 def test_romans_for_negatives_or_zero():
     assert romans(-1) == "not possible to convert " \
                          "-1 to romans"
     assert romans(0) == "not possible to convert 0 to romans"
+
+
+def test_romans_for_2():
+    assert romans(2) == "II"
+
+
+def test_romans_for_3():
+    assert romans(3) == "III"
+
