99a0844c68a431e0299286dc86c1a11292a2b991
JanetBarroso <47856788+JanetBarroso@users.noreply.github.com>
2020-09-28 13:27:15 +0200

    final version that converts arabics into romans

--- tests/test_romanos.py
+++ tests/test_romanos.py
@@ -34,3 +34,29 @@ def test_replace5C_w_D():
 
 def test_replace2D_w_M():
     assert arabic2roman(3112) == "MMMCXII"
+
+
+def test_replace4_w_IV():
+    assert arabic2roman(4) == "IV"
+    assert arabic2roman(14) == "XIV"
+    assert arabic2roman(64) == "LXIV"
+
+def test_replace9_w_IX():
+    assert arabic2roman(9) == "IX"
+    assert arabic2roman(39) == "XXXIX"
+
+def test_replace_40_w_XL():
+    assert arabic2roman(40) == "XL"
+    assert arabic2roman(49) == "XLIX"
+
+def test_replace_90_w_XL():
+    assert arabic2roman(90) == "XC"
+    assert arabic2roman(199) == "CXCIX"
+
+def test_replace_400_w_CD():
+    assert arabic2roman(492) == "CDXCII"
+    assert arabic2roman(438) == "CDXXXVIII"
+
+def test_replace_900_w_CM():
+    assert arabic2roman(983) == "CMLXXXIII"
+    assert arabic2roman(902) == "CMII"
