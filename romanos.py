0ee150d9260404cbd120d748c521a88609f7e863
JanetBarroso <47856788+JanetBarroso@users.noreply.github.com>
2020-09-28 12:58:06 +0200

    converts arabic2romans except 4s 9s and their multiples

--- romanos.py
+++ romanos.py
@@ -1,59 +1,23 @@
 
-EQUIVALENTS = {1: "I", 5: "V", 10: "X", 50: "L",
-               100: "C", 500: "D", 1000: "M"}
-EQ_VAL = list(EQUIVALENTS)
+def arabic2roman(entero):
 
-
-def romans(entero):
     if entero <= 0:
         return "not possible to convert " + \
                str(entero) + " to romans"
-    if entero <= 3:
-        roman = EQUIVALENTS[1] * entero
-    elif entero == 4:
-        roman = "IV"
-    elif entero == 6:
-        roman = "VI"
-    else:
-        roman = EQUIVALENTS[entero]
+
+    roman = entero * "I"
+
+    if entero >= 5:
+        nV = int(entero / 5)
+        roman = roman.replace(5*nV*"I", nV*"V")
+        nX = int(nV / 2)
+        roman = roman.replace(2*nX*"V", nX*"X")
+        nL = int(nX / 5)
+        roman = roman.replace(5*nL*"X", nL*"L")
+        nC = int(nL / 2)
+        roman = roman.replace(2*nC*"L", nC*"C")
+        nD =int(nC / 5)
+        roman = roman.replace(5*nD*"C", nD*"D")
+        nM = int(nD / 2)
+        roman = roman.replace(2*nM*"D", nM*"M")
     return roman
-
-
-def haz_palitos(entero):
-    return EQUIVALENTS[1] * entero
-
-
-def string_patterns(string_palitos):
-    cuantos_palitos = string_palitos.count("I")
-    if cuantos_palitos in EQ_VAL:
-        return string_palitos.replace(
-            string_palitos, EQUIVALENTS[cuantos_palitos]
-        )
-    elif EQ_VAL[0] < cuantos_palitos < EQ_VAL[1]:
-        how_many_I = int(cuantos_palitos / EQ_VAL[1])
-        resto = cuantos_palitos - (how_many_I * EQ_VAL[0])
-        if resto > 3:
-            resto = resto - 3
-            return haz_palitos(resto) + "V"
-
-    elif EQ_VAL[1] < cuantos_palitos < EQ_VAL[2]:
-        how_many_V = int(cuantos_palitos / 5)
-        resto = cuantos_palitos - (how_many_V * 5)
-        if resto > 3:
-            resto = resto - 3
-            return haz_palitos(resto) + "X"
-
-        return "V" * how_many_V + haz_palitos(resto)
-
-    elif EQ_VAL[2] < cuantos_palitos < EQ_VAL[3]:
-        how_many_X = int(cuantos_palitos / 10)
-        resto = cuantos_palitos - (how_many_X * 10)#9
-        how_many_V = int(resto / 5)
-        resto = resto - (how_many_V * 5)
-        if resto > 3:
-            resto = resto-3
-            return "X"*how_many_X + haz_palitos(resto) + "V"
-
-        return "X"*how_many_X + "V"*how_many_V + haz_palitos(resto)
-
-    return string_palitos
--- tests/test_romanos.py
+++ tests/test_romanos.py
@@ -1,76 +1,36 @@
-from romanos import romans, haz_palitos, string_patterns
-
-
-def test_romans_for_one_is_I():
-    assert romans(1) == "I"
-
-
-def test_romans_for_5_to_V():
-    assert romans(5) == "V"
+from romanos import arabic2roman
 
 
 def test_romans_for_negatives_or_zero():
-    assert romans(-1) == "not possible to convert " \
-                         "-1 to romans"
-    assert romans(0) == "not possible to convert 0 to romans"
+    assert arabic2roman(-1) == "not possible to convert " \
+                               "-1 to romans"
+    assert arabic2roman(0) == "not possible to convert 0 to romans"
 
 
-def test_romans_for_2():
-    assert romans(2) == "II"
+def test_print_EnteroPalitos():
+    assert arabic2roman(2) == "II"
 
 
-def test_romans_for_3():
-    assert romans(3) == "III"
+def test_replace5palitos_w_V():
+    assert arabic2roman(6) == "VI"
+    assert arabic2roman(8) == "VIII"
 
 
-def test_romans_for_4():
-    assert romans(4) == "IV"
+def test_replace2V_w_X():
+    assert arabic2roman(10) == "X"
+    assert arabic2roman(36) == "XXXVI"
 
-def test_romans_for_5():
-    assert romans(5) == "V"
+def test_replace5X_w_L():
+    assert arabic2roman(52) == "LII"
+    assert arabic2roman(66) == "LXVI"
 
+def test_replace2L_w_C():
+    assert arabic2roman(103) == "CIII"
+    assert arabic2roman(376) == "CCCLXXVI"
 
-def test_romans_for_6():
-    assert romans(6) == "VI"
+def test_replace5C_w_D():
+    assert arabic2roman(503) == "DIII"
+    assert arabic2roman(787) == "DCCLXXXVII"
 
-def test_haz_palitos():
-    assert haz_palitos(3) == "III"
-
-
-def test_string_patterns():
-    assert string_patterns("III") == "III"
-    assert string_patterns("IIIII") == "V"
-    assert string_patterns("IIIIIIIIII") == "X"
-
-
-def test_replace_6_8_palitos_w_V():
-    assert (string_patterns("IIIIII")) == "VI"
-    assert (string_patterns("IIIIIII")) == "VII"
-    assert (string_patterns("IIIIIIII")) == "VIII"
-    assert (string_patterns("IIIIIIIII")) =="IX"
-
-def test_romans_from1_to5():
-    assert (string_patterns("IIII")) == "IV"
-    assert (string_patterns("I")) == "I"
-    assert (string_patterns("II")) == "II"
-
-def test_replace_palitos_w_X_():
-    assert(string_patterns("IIIIIIIIIII")) == "XI"
-    assert (string_patterns("IIIIIIIIIIIIIII")) =="XV"
-    assert (string_patterns("IIIIIIIIIIIIIIII")) == "XVI"
-    assert string_patterns("IIIIIIIIIIIIII") == "XIV"
-
-def test_33():
-    assert (string_patterns("IIIIIIIIIIII"
-                            "IIIIIIIIIIII"
-                            "IIIIIIIII")) == "XXXIII"
-
-def test_36():
-    assert (string_patterns("IIIIIIIIIIIII"
-                            "IIIIIIIIIIIII"
-                            "IIIIIIII")) == "XXXIV"
-
-def test_39():
-    assert (string_patterns("IIIIIIIIIIIII"
-                            "IIIIIIIIIIIII"
-                            "IIIIIIIIIIIII")) == "XXXIX"
+def test_replace2D_w_M():
+    assert arabic2roman(3112) == "MMMCXII"
